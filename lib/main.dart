635568a27fb01065f0eb34bc3af8586d8957887f
Yashwant Singh <yashwantsinghbtp@gmail.com>
2020-03-29 23:45:11 +0530

    connected the app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
+        // Try running your application with "flutter run". You'll s11111ee the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
