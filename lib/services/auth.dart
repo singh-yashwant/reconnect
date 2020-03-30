77ad772fbfedf9e0b6b88346f23c192443faa032
Yashwant Singh <yashwantsinghbtp@gmail.com>
2020-03-31 00:09:43 +0530

    users database created and linked with registerarion method

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:reconnect/models/user_model.dart';
 
 class AuthService {
 
 	final FirebaseAuth _auth = FirebaseAuth.instance;
+	final db = Firestore.instance;
 
 	// create user object from firebase user object
-	User _userFromFirebaseUser(FirebaseUser user){
+	User _userFromFirebaseUser(FirebaseUser user) {
 		return user != null ? User(uid: user.uid) : null;
 	}
 
@@ -40,13 +42,18 @@ class AuthService {
 		}
 	}
 
-	// register with email and
-	Future registerWithEmailAndPassword(String email, String password) async{
+	// register with email and password
+	Future registerWithEmailAndPassword(String email, String password, String name,
+			String school, String college, String school_batch, String college_batch) async{
 		try{
 			AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
 			FirebaseUser user = result.user;
+
+			createEntryInUsersCollection(user, email, password, name, school, college, school_batch, college_batch);
+
 			return _userFromFirebaseUser(user);
-		}catch(e){
+		}
+		catch(e){
 			print(e.toString());
 			return null;
 		}
@@ -60,6 +67,28 @@ class AuthService {
 		catch(e){
 			print(e.toString());
 			return null;
+		}
+	}
+
+	// create a entry in firestore users db
+	Future createEntryInUsersCollection(FirebaseUser user, String email, String password, String name,
+			String school, String college, String school_batch, String college_batch) async {
+
+		try {
+			await Firestore.instance.collection("users").document(user.uid.toString())
+					.setData({
+				"uid": user.uid.toString(),
+				"name": name,
+				"email": email,
+				"password": password,
+				"school": school,
+				"school_batch": school_batch,
+				"college": college,
+				"college": college_batch,
+			});
+		}catch(e){
+			print("------unable to write to firestore\n");
+			print(e);
 		}
 	}
 
